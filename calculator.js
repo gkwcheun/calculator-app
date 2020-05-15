83d32deb65fec82803c05e1769fc4585cc446b40
Galvin <galvincheung@gmail.com>
2020-05-15 11:34:59 -0600

    added keydown functionality

--- calculator.js
+++ calculator.js
@@ -1,35 +1,11 @@
-// class Calculator {
+let expression = "";
+let out = document.getElementById('out');
+let buttonList = document.querySelectorAll('button');
+let buttonArr = Array.from(buttonList);
+let buttonKeys = buttonArr.map((button) => (button.dataset.key));
+let specialKeys = ['Enter', 'Backspace', 'Escape'];
 
-//     constructor() {
-//         this.result = 0;
-//     }
-//     add(b) {
-//         this.result += b;
-//         return this.result
-//     }
-//     subtract(b) {
-//         this.result -= b;
-//         return this.result
-//     }
-//     multiply(b) {
-//         this.result *= b;
-//         return this.result
-//     }
-//     divide(b) {
-//         this.result /= b;
-//         return this.result
-//     }
-//     clear() {
-//         this.result = 0;
-//         return this.result
-//     }
-//     setFirstNum(a) {
-//         this.result = a;
-//     }
-
-// }
-// let calc = new Calculator();
-// let currentResult = 0;
+out.innerText = 0;
 
 function updateLive(expression) {
     let live = document.getElementById('live');
@@ -37,28 +13,55 @@ function updateLive(expression) {
     return
 }
 
+// sets the output p tag with the output of calculation
 function setOutput(out) {
     let output = document.getElementById('out');
     output.innerText = out;
 }
 
+// deletes last single entry in calculator, ran when backspace is pressed
+// or when ce is clicked
 function deleteLast(expression) {
     expression = expression.substr(0, expression.length - 1);
     return expression
 }
 
+// ran when enter or equal button is pressed or clicked
 function calculate(expression) {
     output = eval(expression);
     return output
 }
 
-let expression = "";
+// check which key was pressed and execute action associated
+function keyCheck(event) {
+    if (buttonKeys.includes(event.key)){
+        if (specialKeys.includes(event.key)) {
+            switch (event.key) {
+                case 'Enter':
+                    output = calculate(expression);
+                    updateLive(output);
+                    setOutput(output);
+                    break;
+                case 'Backspace':
+                    expression = deleteLast(expression);
+                    updateLive(expression);
+                    break;
+                case 'Escape':
+                    expression = "";
+                    output = "0";
+                    updateLive(expression);
+                    setOutput(output);
+                    break;
+            }
+        }
+        else if (expression.length < 17) {
+            expression += event.key;
+            updateLive(expression);
+        }
+    }
+} 
 
-let out = document.getElementById('out');
-out.innerText = 0;
-
-let buttonList = document.querySelectorAll('button');
-
+// event listeners for click and key downs
 buttonList.forEach((button) => (
     button.addEventListener('click', () => {
         if (button.id != 'ce' && button.id != 'ac' && button.id != 'equal') {
@@ -73,7 +76,7 @@ buttonList.forEach((button) => (
         }
         else if (button.id == 'ac') {
             expression = "";
-            output = "";
+            output = "0";
             updateLive(expression);
             setOutput(output);
         }
@@ -85,3 +88,4 @@ buttonList.forEach((button) => (
     })
 ))
 
+window.addEventListener('keydown', keyCheck);
